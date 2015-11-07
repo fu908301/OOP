681791578ddc5f9a8c89c994985d1d6e797983e5
fu908301 <fu908301@gmail.com>
2015-11-07 16:29:30 +0800

    HW3 V1.1 almost done.Just waiting for standard output string

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -15,6 +15,7 @@ class rational{
   private:
     int numerator,denominator;
 };
+void gcd(int num1,int num2);
 rational::rational(int _num,int _den)
 {
   numerator = _num;
@@ -51,6 +52,38 @@ void rational::gcd()
     denominator *= -1;
   }
 }
+rational operator +(rational& ra1,rational& ra2)
+{
+  int result_nu,result_de;
+  result_de = ra1.denominator * ra2.denominator;
+  result_nu = ra1.numerator * ra2.denominator + ra2.numerator * ra1.denominator;
+  gcd(result_nu,result_de);
+  return rational(result_nu,result_de);
+}
+rational operator -(rational& ra1,rational& ra2)
+{
+  int result_nu,result_de;
+  result_de = ra1.denominator * ra2.denominator;
+  result_nu = ra1.numerator * ra2.denominator - ra2.numerator * ra1.denominator;
+  gcd(result_nu,result_de);
+  return rational(result_nu,result_de);
+}
+rational operator *(rational& ra1,rational& ra2)
+{
+  int result_nu,result_de;
+  result_nu = ra1.numerator * ra2.numerator;
+  result_de = ra1.denominator * ra2.denominator;
+  gcd(result_nu,result_de);
+  return rational(result_nu,result_de);
+}
+rational operator /(rational& ra1,rational& ra2)
+{
+ int result_nu,result_de;
+ result_nu = ra1.numerator * ra2.denominator;
+ result_de = ra1.denominator * ra2.numerator;
+ gcd(result_nu,result_de);
+ return rational(result_nu,result_de);
+}
 istream& operator >>(istream& input,rational& ra)
 {
   int topnumber,buttonnumber;
@@ -73,11 +106,44 @@ ostream& operator <<(ostream& output,rational& ra)
     output<<ra.numerator<<"/"<<ra.denominator<<"\n";
   return output;
 }
+void gcd(int num1,int num2)
+{
+  int a = num1,b = num2,r;
+  if(a<0)
+    a *= -1;
+  if(b<0)
+    b *= -1;
+  while(b>0)
+  {
+    r = a%b;
+    a = b;
+    b = r;
+  }
+  num1 /= a;
+  num2 /= a;
+  if((num1<0&&num2<0)||(num1>0&&num2<0))
+  {
+    num1 *= -1;
+    num2 *= -1;
+  }
+}
 int main()
 {
-  rational a;
+  rational a,b,result1,result2,result3,result4;
+  cout<<"Please input 2 numbers"<<endl;
   cin>>a;
+  cin>>b;
   a.gcd();
-  cout<<a;
+  b.gcd();
+  cout<<"A="<<a;
+  cout<<"B="<<b;
+  result1 = a+b;
+  result2 = a-b;
+  result3 = a*b;
+  result4 = a/b;
+  cout<<"A+B="<<result1;
+  cout<<"A-B="<<result2;
+  cout<<"A*B="<<result3;
+  cout<<"A/B="<<result4;
   return 0;
 }
